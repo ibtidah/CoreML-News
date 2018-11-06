66f3b9d3c6d09d1a031d3d7f9165b334cd43491e
tallal <tallal.javed@outlook.com>
2018-11-06 12:55:28 +0500

    Initial Commit

---
+++ NewsML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsML
+//
+//  Created by Tallal Javed on 11/6/18.
+//  Copyright © 2018 Tallal Javed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
